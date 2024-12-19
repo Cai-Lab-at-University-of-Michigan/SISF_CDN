bddf70f9cad2f03eb8809b8e7dbd98ab7306ef7a
Logan Walker <loganaw@umich.edu>
2024-12-19 11:09:35 -0500

    Refactor ts differentiation to reader util functions

--- src/app.cpp
+++ src/app.cpp
@@ -1653,18 +1653,12 @@ int main(int argc, char *argv[])
 		//uint16_t out_buffer[handler->channel_count][chunk_sizes[2]][chunk_sizes[1]][chunk_sizes[0]];
 		const size_t out_buffer_size = sizeof(uint16_t) * chunk_sizes[0] * chunk_sizes[1] * chunk_sizes[2] * reader->channel_count;
 
-		uint16_t * out_buffer;
-		if(reader->type == SISF) {
-			out_buffer = reader->load_region(
-				scale,
-				x_begin, x_end,
-				y_begin, y_end,
-				z_begin, z_end
-			);
-		} else if(reader->type == ZARR) {
-			//TODO
-			out_buffer = (uint16_t*) malloc(out_buffer_size);
-		}
+		uint16_t * out_buffer = reader->load_region(
+			scale,
+			x_begin, x_end,
+			y_begin, y_end,
+			z_begin, z_end
+		);
 
 		for(const auto& pair : filters) {
 			filter_run(
