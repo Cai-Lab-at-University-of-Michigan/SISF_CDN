f12d784e34805fff2a70cd4026124b8f22c23a40
Logan Walker <loganaw@umich.edu>
2024-12-19 12:41:35 -0500

    Update reader.hpp

--- src/reader.hpp
+++ src/reader.hpp
@@ -831,7 +831,7 @@ public:
                                                                ((j - ys) * osizex) +            // Y
                                                                ((i - xs));                      // X
 
-                                        out_buffer[ooffset] = *array.operator()((tensorstore::Index)i, (tensorstore::Index)j, (tensorstore::Index)k, (tensorstore::Index)c);
+                                        out_buffer[ooffset] = ((uint16_t *) array.operator()((tensorstore::Index)i, (tensorstore::Index)j, (tensorstore::Index)k, (tensorstore::Index)c))[0];
                                     }
                                 }
                             }
