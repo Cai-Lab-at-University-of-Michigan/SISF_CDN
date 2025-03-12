aacaa12a9537e38e1f63f16314483e8a45b78d9c
Logan Walker <loganaw@umich.edu>
2025-03-11 21:32:26 -0400

    Update utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -274,7 +274,8 @@ void clahe_1d(uint16_t *image, size_t data_size, uint32_t clipLimit)
     for (size_t i = 0; i < data_size / sizeof(uint16_t); i++)
     {
         const int p = image[i];
-        int bin = (p * bins); //  / (int)std::numeric_limits<uint16_t>::max();
+        int bin = p; //  / (int)std::numeric_limits<uint16_t>::max();
+        bin /= bins;
         hist[bin] += 1;
     }
 
