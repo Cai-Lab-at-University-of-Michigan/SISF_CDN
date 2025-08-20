212dcf93fbe48aee5545ef89a491db2d9b99e04e
Logan Walker <loganaw@umich.edu>
2025-08-20 11:33:56 -0400

    Initial merge of vidlib native changes

--- src/reader.hpp
+++ src/reader.hpp
@@ -339,7 +339,8 @@ public:
 
             case 3:
                 // Decompress with vidlib 2
-                read_decomp_buffer_pt = decode_stack_AV1(sizex, sizey, sizez, read_buffer, sel->size);
+                //read_decomp_buffer_pt = decode_stack_AV1(sizex, sizey, sizez, read_buffer, sel->size);
+		read_decomp_buffer_pt = decode_stack_native(sizex, sizey, sizez, read_buffer, sel->size);
                 pix_cnt = sizex * sizey * sizez;
                 decomp_size = pix_cnt * sizeof(uint16_t);
                 read_decomp_buffer = (char *)pixtype_to_uint16(read_decomp_buffer_pt, pix_cnt);
